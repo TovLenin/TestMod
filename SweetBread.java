ebe37536c23fda45c77ba3d6c649cb54780a2aba
TovLenin <alikmih2011@gmail.com>
2013-11-15 22:11:49 +0300

    Create SweetBread.java

---
+++ SweetBread.java
@@ -0,0 +1,19 @@
+package assets.testmod.src;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemFood;
+
+public class SweetBread extends ItemFood
+{
+public SweetBread(int par1, int par2, float par3, boolean par4)
+{
+super(par1, par2, par4);
+maxStackSize = 64;
+this.setCreativeTab(CreativeTabs.tabFood);
+}
+@Override
+public void registerIcons(IconRegister par1IconRegister)
+{
+                                this.itemIcon = par1IconRegister.registerIcon(ModInfo.MODID + ":sweetBread");
+}
+}
