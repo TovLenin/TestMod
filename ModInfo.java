fd7a5ede53f583e6dae070f3b8c27f89ecb4417b
TovLenin <alikmih2011@gmail.com>
2013-11-15 22:10:37 +0300

    Create ModInfo.java

---
+++ ModInfo.java
@@ -0,0 +1,16 @@
+package assets.testmod.src;
+
+public class ModInfo {
+    /*
+    * ID вашего мода
+    */
+    public final static String MODID = "TestMod";
+    /*
+    * Имя вашего мода
+    */
+    final static String NAME = "Test Mod";
+    /*
+    * Версия вашего мода
+    */
+    final static String VERSION = "1.0.0";
+}
