6d16beb75d23a173f6260955cc22ef896ab83b6a
TovLenin <alikmih2011@gmail.com>
2013-11-16 19:27:58 +0300

    Update SweetBread.java

--- SweetBread.java
+++ SweetBread.java
@@ -5,15 +5,16 @@ import net.minecraft.item.ItemFood;
 
 public class SweetBread extends ItemFood
 {
-public SweetBread(int par1, int par2, float par3, boolean par4)
-{
-super(par1, par2, par4);
-maxStackSize = 64;
-this.setCreativeTab(CreativeTabs.tabFood);
-}
-@Override
-public void registerIcons(IconRegister par1IconRegister)
-{
-                                this.itemIcon = par1IconRegister.registerIcon(ModInfo.MODID + ":sweetBread");
-}
+	public SweetBread(int par1, int par2, float par3, boolean par4)
+	{
+		super(par1, par2, par4);
+		maxStackSize = 64;
+		this.setCreativeTab(CreativeTabs.tabFood);
+	}
+	
+	@Override
+	public void registerIcons(IconRegister par1IconRegister)
+		{
+			this.itemIcon = par1IconRegister.registerIcon(ModInfo.MODID + ":sweetBread");
+		}
 }
