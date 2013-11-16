819e61f758909dee98cb1111e86d908834a860a7
TovLenin <alikmih2011@gmail.com>
2013-11-16 14:32:01 +0300

    Update Testmod.java

--- Testmod.java
+++ Testmod.java
@@ -1,6 +1,5 @@
 package assets.testmod.src;
 
-import assets.testmod.src.Fork;
 import net.minecraft.block.Block;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
@@ -16,9 +15,9 @@ import net.minecraftforge.common.EnumHelper;
 @Mod(modid=ModInfo.MODID, name=ModInfo.NAME, version=ModInfo.VERSION)
 
 public class TestMod {
-	public static Item fork = new Fork(3200).setUnlocalizedName("ChistiСhisti");
+	public static Item fork = new Fork(3200).setUnlocalizedName("Чисти-чисти");
 	public static final Block testBlock = new TestBlock(2000).setUnlocalizedName("testBlock");
-	public static Item sladkyHleb = new SweetBread(2022, 1, 0.4F, false).setUnlocalizedName("Sweet bread");
+	public static Item sladkyHleb = new SweetBread(2022, 5, 0.4F, false).setUnlocalizedName("Sweet bread");
 	public static final EnumToolMaterial TEST = EnumHelper.addToolMaterial("TEST", 4, 2000, 10.0F, 5.0F, 10);
 	public static final Block shitOre = new ShitOre(2001).setUnlocalizedName("shitOre");
 	@Mod.Instance(ModInfo.MODID)
