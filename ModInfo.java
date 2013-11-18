bb323b1fbaf814cf600c24e0579fa84bb1911be9
TovLenin <alikmih2011@gmail.com>
2013-11-18 15:28:03 +0300

    Update ModInfo.java

--- ModInfo.java
+++ ModInfo.java
@@ -1,16 +1,7 @@
 package assets.testmod.src;
 
 public class ModInfo {
-    /*
-    * ID вашего мода
-    */
     public final static String MODID = "TestMod";
-    /*
-    * Имя вашего мода
-    */
     final static String NAME = "Test Mod";
-    /*
-    * Версия вашего мода
-    */
     final static String VERSION = "1.0.0";
 }
